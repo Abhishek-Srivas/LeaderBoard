906a2bd0be964f3d97dce17565fb9d0bf83e97ef
h01sachan <sachan.himanshu2001@gmail.com>
2022-02-14 17:15:10 +0530

    FEAT : TOP 10 RANK API DONE

--- controllers/main.js
+++ controllers/main.js
@@ -11,6 +11,8 @@ const {
 } = require("unique-names-generator");
 
 const redisClient = require("../helpers/redis");
+const leaderboard = require("../models/leaderboard");
+const promise = require("bluebird/js/release/promise");
 
 exports.search = async (req, res, next) => {
   try {
@@ -23,6 +25,7 @@ exports.search = async (req, res, next) => {
   }
 };
 
+//added 1 million user
 exports.addUser = async (req, res, next) => {
   try {
     // const { name } = req.body;
@@ -31,7 +34,7 @@ exports.addUser = async (req, res, next) => {
 
     // Inserting 10K users
     const data = [];
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 100000; i++) {
       const randomName = uniqueNamesGenerator({
         dictionaries: [adjectives, animals],
       });
@@ -44,33 +47,34 @@ exports.addUser = async (req, res, next) => {
       leaderBoardData.push({ id: userData._id });
     });
     const Ldata = await LeaderBoard.insertMany(leaderBoardData);
-    console.log(Ldata);
+   // console.log(Ldata);
     res.status(201).json({ success: true, data: "User Added" });
   } catch (err) {
     next(err);
   }
 };
 
-exports.updatePoints = async (req, res, next) => {
+exports.addToCache = async (req, res, next) => {
   try {
-    const { user } = req.query;
-    const { newPoint } = req.body;
-
-    const newRecord = await LeaderBoard.findOneAndUpdate(
-      { id: user },
-      {
-        $inc: { points: newPoint },
-      },
-      { new: true }
-    ).populate("id");
-
     redisClient.then(async (client) => {
       const zadd = promisify(client.zadd).bind(client);
       const hmset = promisify(client.hmset).bind(client);
-      const result = await hmset(user, "name", newRecord.id.name);
-      const data = await zadd("leaderboard", JSON.stringify(newRecord.points), user);
+      const tt = await leaderboard.find().populate("id").limit(40000).skip(60000);
+      console.log(1);
+      pt = []
+      for (let i = 0; i < 40000; i += 1) {
+        const t1 = zadd("leaderboard", JSON.stringify((tt[i].points)), tt[i].id.id);
+        const t2 = hmset(tt[i].id.id, "name", tt[i].id.name);
+        const t3 = zadd("score", JSON.stringify((tt[i].points)), JSON.stringify((tt[i].points)));
+        pt.push(t1);
+        pt.push(t2);
+        pt.push(t3);
+      }
+      console.log(2);
+      await Promise.all(pt);
+      console.log(3)
 
-      return res.json(data);
+      return res.json("OK");
     });
   } catch (err) {
     console.log(err);
@@ -81,20 +85,11 @@ exports.getLeaderboard = async (req, res, next) => {
   try {
     redisClient.then(async (client) => {
       const zrevrange = promisify(client.zrevrange).bind(client);
-      const hgetall = promisify(client.hgetall).bind(client);
-      let rank = []
-      const data = await zrevrange("leaderboard", 0, -1, 'withscores');
-      for (let i=0;i<data.length;i+=2)
-      {
-        const user_details = await hgetall(data[i]);
-        rank.push({
-          name : user_details.name,
-          id : data[i],
-          score : data[i+1],
-          rank : Math.floor((i+1)/2)+1
-        })
-      }
-      return res.json(rank);
+      const hmget = promisify(client.hmget).bind(client);
+      const zrank = promisify(client.zrank).bind(client);
+      let rank = [];
+      const data = await zrevrange("leaderboard", 0, 10, 'withscores');
+      return res.json(data);
     })
   } catch (error) {
     next(error);
@@ -157,9 +152,73 @@ exports.dummyData = async (req, res, next) => {
   }
 };
 
-exports.getTopTen = async(req,res,next)=>{
+exports.getTopTen = async (req, res, next) => {
   try {
-    
+    const { user } = req.query;
+    redisClient.then(async (client) => {
+
+      const zrevrange = promisify(client.zrevrange).bind(client);
+      const hget = promisify(client.hget).bind(client);
+      const zscore = promisify(client.zscore).bind(client);
+      const zrevrank = promisify(client.zrevrank).bind(client);
+      const t1 = zscore("leaderboard", user);
+      const t2 = hget(user, "name");
+      const t3 = zrevrank("score", user);
+      const t4 = zrevrange("leaderboard", 0, 9, 'withscores')
+      const t5 = zrevrange("leaderboard", 0, 9)
+      const [user_score, user_name, user_rank, ranks, userids] = await Promise.all([t1, t2, t3, t4, t5]);
+      const data = [];
+      let startRank = 1, pre = -1;
+      for (let i = 0; i < ranks.length; i += 2) {
+        data.push({
+          rank: startRank,
+          userid: ranks[i], //comapny id
+          score: ranks[i + 1],
+        });
+        if (pre != ranks[i + 1]) startRank += 1;
+        pre = ranks[i + 1]
+      }
+      const names = await Promise.all(userids.map((id) => {
+        return hget(id, "name")
+      }));
+      //console.log(data);
+      const results = names.map((name, index) => ({
+        name: name,
+        score: data[index].score,
+        rank: data[index].rank,
+        userid: data[index].userid,
+      }))
+      res.json({
+        results,
+        user_name,
+        user_score,
+        user_rank: user_rank + 1
+      });
+      // const zscore = promisify(client.zscore).bind(client);
+      // const zrevrank = promisify(client.zrevrank).bind(client);
+      // const rank = []
+      // const data = zrevange("leaderboard", 0, 9, 'withscores'); //withscores
+      // const data1 = zrange("leaderboard", 0, 10); //only user ids
+      // const score = zrange("score", 0, 10); //score 
+      // const user_score = zscore("leaderboard", user);
+      // const user_name = hmget(user, "name");
+      // const [t1, t2, t3, t4, t5] = await Promise.all([data, score, data1, user_score, user_name]);
+      // console.log(t4);
+      // // console.log(data);
+      // const promises = t3.map((userid) => {
+      //   //console.log(userid)
+      //   return hmget(userid, "name")
+      // });
+      // const names = await Promise.all(promises);
+      // //console.log(names)
+      // return res.json({
+      //   data: t1, score: t2, names: names, user: {
+      //     name: t5[0],
+      //     score: t4,
+      //     id: user
+      //   }
+      // });
+    })
   } catch (error) {
     next(error);
   }
--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,9 @@ const mainController = require('../controllers/main')
 
 router.use('/adduser',mainController.addUser);
 router.use('/testing',mainController.search);
-router.use('/update-points',mainController.updatePoints);
+router.use('/update-points',mainController.addToCache);
 router.get("/leaderboard",mainController.getLeaderboard);
 router.get("/dummyData",mainController.dummyData);
+router.get("/top10",mainController.getTopTen);
 
 module.exports = router;
